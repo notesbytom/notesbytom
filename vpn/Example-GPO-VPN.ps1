bc312a376a212f8c44475f194cefc1e67704bd64
notesbytom <notesbytom@users.noreply.github.com>
2020-11-12 15:47:07 -0500

    Update Example-GPO-VPN.ps1
    Switch to EAP (default EAP-MSCHAPv2). Add variables for server, psk, and auth type.

--- vpn/Example-GPO-VPN.ps1
+++ vpn/Example-GPO-VPN.ps1
@@ -1,9 +1,13 @@
 # Readable Example Adding L2TP IPSec VPN Connection with Pre-Shared Key (PowerShell)
 # ... This sample will prompt for credentials when connecting.
 # ... Change values of $name and $options to match your environment or test scenario
+# ... Default EAP Setting is EAP-MSCHAPv2
 
 $name = 'Test-VPN';
-$options = @{AllUserConnection=$true; Name=$name; ServerAddress='vpnserver.yourdomain.com'; TunnelType='L2tp'; EncryptionLevel='Maximum'; AuthenticationMethod='MSChapv2'; UseWinlogonCredential=$false; L2tpPsk='Your.Pre-Shared-Key.Here.Abc.123'; IdleDisconnectSeconds=14400; Force=$true};
+$server = 'vpnserver.yourdomain.com';
+$psk = 'Your.Pre-Shared-Key.Here.Abc.123';
+$auth = 'Eap';
+$options = @{AllUserConnection=$true; Name=$name; ServerAddress=$server; TunnelType='L2tp'; EncryptionLevel='Maximum'; AuthenticationMethod=$auth; UseWinlogonCredential=$false; RememberCredential=$false; L2tpPsk=$psk; IdleDisconnectSeconds=14400; Force=$true};
 $count = 0;
 foreach ($conn in get-vpnconnection -alluserconnection) {
     if($conn.Name -eq $name) {
@@ -18,4 +22,4 @@ if ($count -lt 1) { Add-VpnConnection @options; };
 # ... Watch out for quoting and escaping ... convert internal quotes to single
 # ... With this same method we can also set EAP policy, custom IPsec, etc.
 
-powershell -command "& {$name = 'Test-VPN'; $options = @{AllUserConnection=$true; Name=$name; ServerAddress='vpnserver.yourdomain.com'; TunnelType='L2tp'; EncryptionLevel='Maximum'; AuthenticationMethod='MSChapv2'; UseWinlogonCredential=$false; L2tpPsk='Your.Pre-Shared-Key.Here.Abc.123'; IdleDisconnectSeconds=14400; Force=$true}; $count = 0; foreach ($conn in get-vpnconnection -alluserconnection) { if($conn.Name -eq $name) { $count += 1; Set-VpnConnection @options; } }; if ($count -lt 1) { Add-VpnConnection @options; };}"
+powershell -command "& {$name = 'Test-VPN'; $server = 'vpnserver.yourdomain.com'; $psk = 'Your.Pre-Shared-Key.Here.Abc.123'; $auth = 'Eap'; $options = @{AllUserConnection=$true; Name=$name; ServerAddress=$server; TunnelType='L2tp'; EncryptionLevel='Maximum'; AuthenticationMethod=$auth; UseWinlogonCredential=$false; RememberCredential=$false; L2tpPsk=$psk; IdleDisconnectSeconds=14400; Force=$true}; $count = 0; foreach ($conn in get-vpnconnection -alluserconnection) { if($conn.Name -eq $name) { $count += 1; Set-VpnConnection @options; } }; if ($count -lt 1) { Add-VpnConnection @options; };}"
