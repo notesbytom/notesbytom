33a5bd6e96e4452946fdee29a1c5f9a23cc69f31
notesbytom <notesbytom@users.noreply.github.com>
2021-03-15 13:38:26 -0400

    Update create_csr.sh
    Add Git Bash comments. Add IP to subjectAltName example.

--- openssl/create_csr.sh
+++ openssl/create_csr.sh
@@ -8,9 +8,12 @@ set -o xtrace
 # set your Primary FQDN as the commonName variable
 # set your Alias FQDN's as comma-separated "DNS: f.q.d.n" entries in altNames variable
 
+# for Git Bash on Windows, prefix Subject with double-slash like: "//CN=..."
+# for Git Bash on Windows, prefix openssl command with winpty: winpty openssl ...
+
 commonName="server.example.com"
 mySubject="/CN=$commonName"
-altNames="DNS: $commonName, DNS: alias.example.com"
+altNames="DNS: $commonName, DNS: alias.example.com, IP: 1.2.3.4"
 
 openssl req \
         -new \
