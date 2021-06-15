90f223600b8c552058ee089972301249cdbfb5d3
notesbytom <notesbytom@users.noreply.github.com>
2021-06-15 13:43:51 -0400

    Create math_convert_network.ps1

---
+++ powershell/math_convert_network.ps1
@@ -0,0 +1,9 @@
+# Example convert 64 Mega Bytes per second to Mega Bits per second network rate
+
+64*[math]::pow(2,20)*8/1e6
+
+# 536.870912
+
+# 1 Mega Byte = 2^20 Bytes
+# 1 Byte = 8 Bits
+# 1 Mega Bit = 10^6 Bits
