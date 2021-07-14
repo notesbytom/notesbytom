a24f1c873a8985f8d4484973a42f86182519599c
notesbytom <notesbytom@users.noreply.github.com>
2021-07-14 12:04:47 -0400

    Create aws_wrap_key.sh
    based on example from https://aws.amazon.com/premiumsupport/knowledge-center/invalidciphertext-kms/

---
+++ openssl/aws/aws_wrap_key.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+set -o xtrace
+
+# example from https://aws.amazon.com/premiumsupport/knowledge-center/invalidciphertext-kms/
+# REPLACE blicKey.bin with the filename of your wrapping key
+
+openssl pkeyutl -in PlaintextKeyMaterial.bin -out EncryptedKeyMaterial.bin -inkey PublicKey.bin -keyform DER \
+            -pubin -encrypt -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256
