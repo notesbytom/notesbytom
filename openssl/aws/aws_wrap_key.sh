0f4de85a9da8840c7593efd6dcf97e6e648156e4
notesbytom <notesbytom@users.noreply.github.com>
2021-07-14 12:22:13 -0400

    Update aws_wrap_key.sh
    fix typo

--- openssl/aws/aws_wrap_key.sh
+++ openssl/aws/aws_wrap_key.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 
 # example from https://aws.amazon.com/premiumsupport/knowledge-center/invalidciphertext-kms/
-# REPLACE blicKey.bin with the filename of your wrapping key
+# REPLACE PulicKey.bin with the filename of your wrapping key
 
 openssl pkeyutl -in PlaintextKeyMaterial.bin -out EncryptedKeyMaterial.bin -inkey PublicKey.bin -keyform DER \
             -pubin -encrypt -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256
