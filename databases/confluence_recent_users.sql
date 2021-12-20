3e358a7eff3ee807381f2617ce6620127e2710be
notesbytom <notesbytom@users.noreply.github.com>
2021-12-20 15:59:25 -0500

    Update confluence_recent_users.sql

--- databases/confluence_recent_users.sql
+++ databases/confluence_recent_users.sql
@@ -1,6 +1,7 @@
 -- list recent Confluence user logins within the current year
 -- replace "confluence" with your confluence-database-name
 -- MySQL is assumed for this query example
+-- See also: https://confluence.atlassian.com/confkb/how-to-identify-inactive-users-in-confluence-214335880.html
 
 USE confluence;
 
