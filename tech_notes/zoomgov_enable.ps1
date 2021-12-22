34811c453f2927131bd48307d32838ff843ae49f
notesbytom <notesbytom@users.noreply.github.com>
2021-12-22 10:43:35 -0500

    Update zoomgov_enable.ps1
    add the registry key (path) if it is missing

--- tech_notes/zoomgov_enable.ps1
+++ tech_notes/zoomgov_enable.ps1
@@ -1,6 +1,9 @@
 # Changing this setting requires admin (uac-elevated) access
 # Enable and verify that zoomgov is selectable in the zoom software.
 
+# Add the registry key (path) if it is missing first!!
+if (-not (Test-Path "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General\")) { New-Item "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General\" -Force }
+
 # Add or update the setting with this:
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General\" -Name EnableCloudSwitch -PropertyType dword -Value 1 -Force
 
