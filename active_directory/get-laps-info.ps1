1a395b44c2584cb6c39c6bd02ed43575fc643957
notesbytom <notesbytom@users.noreply.github.com>
2022-01-31 11:48:32 -0500

    Update get-laps-info.ps1
    comment on local-admin sid with link to vendor documentation.

--- active_directory/get-laps-info.ps1
+++ active_directory/get-laps-info.ps1
@@ -6,3 +6,7 @@
 [datetime]::FromFileTimeUtc((get-adcomputer $env:COMPUTERNAME -Properties 'ms-Mcs-AdmPwdExpirationTime').'ms-Mcs-AdmPwdExpirationTime')
 get-localgroupmember Administrators
 get-localuser | where sid -like 's-1-5-*-500'
+
+# https://docs.microsoft.com/en-us/windows/security/identity-protection/access-control/security-identifiers#well-known-sids
+# Well-known sid for local admin: S-1-5-domain-500. 
+# "A user account for the system administrator. Every computer has a local Administrator account ..."
