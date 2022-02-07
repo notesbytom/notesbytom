86757b9cabb2b71c49fcf44782d97abb4766f38f
notesbytom <notesbytom@users.noreply.github.com>
2022-02-07 10:46:17 -0500

    Update get-laps-info.ps1
    add function to supply computer name with a default of local computer.

--- active_directory/get-laps-info.ps1
+++ active_directory/get-laps-info.ps1
@@ -10,3 +10,10 @@ get-localuser | where sid -like 's-1-5-*-500'
 # https://docs.microsoft.com/en-us/windows/security/identity-protection/access-control/security-identifiers#well-known-sids
 # Well-known sid for local admin: S-1-5-domain-500. 
 # "A user account for the system administrator. Every computer has a local Administrator account ..."
+
+function get-laps($hostName = $env:COMPUTERNAME) {
+  $adComputer = get-adcomputer $env:COMPUTERNAME -Properties 'ms-Mcs-AdmPwd','ms-Mcs-AdmPwdExpirationTime'
+  "ADComputer Identity = $hostName"
+  "ms-Mcs-AdmPwd = $($adComputer.'ms-Mcs-AdmPwd')"
+  "ms-Mcs-AdmPwdExpirationTime = $([datetime]::FromFileTimeUtc($adComputer.'ms-Mcs-AdmPwdExpirationTime'))"
+}
